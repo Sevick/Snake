c7d85efcf4a8b89d7d3c05d3ecd68461b47f7a1c
Sevick <sevick.v@gmail.com>
2016-10-27 20:27:26 +0300

    Start

---
+++ index.js
@@ -0,0 +1,552 @@
+var express = require('express'); // Get the module
+var app = express(); // 
+var path = require('path');
+
+//var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function (req, res) {
+    //res.send('<h1>Your user ID: </h1>');
+    res.sendfile('index.html');
+});
+
+var players = [];
+var commandsStack = [];
+var gameobjs = [];
+var field = [];   // fieldWidth*fieldHeight  [GameObject 'objecttype']
+
+var fieldWidth = 100;
+var fieldHeight = 50;
+var spawnMargin = 10;
+var userId = 10;
+var gameUpdateDelay = 100;
+var lastGameObjId = 100;
+var bonusLimit = 25;
+var bonusCount = 0;
+var bonusDelay = 5; // in game ticks
+var bonusesTypes = [{color: "green", grow: 1, score: 5, type: 'bonus'},
+    {color: "yellow", grow: 3, score: 10, type: 'bonus'},
+    {color: "white", grow: 5, score: 15, type: 'bonus'}];
+
+var gamePaused = false;
+var tickNumber = 0;
+
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+    field = getClearField();
+});
+
+
+function Point(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+function CellValue(id, color) {
+    this.id = id;
+    this.color = color;
+}
+
+function getNextObjId() {
+    lastGameObjId++;
+    return (lastGameObjId);
+}
+
+function movePoint(point, direction) {
+    var deltas = getDeltas(direction);
+    var newPoint=new Object();
+    newPoint.x = point.x + deltas.deltaX;
+    newPoint.y = point.y + deltas.deltaY;
+    return (newPoint);
+}
+
+//----------------------------------------
+function newBonus() {
+    //console.log("newBonus");
+    var point = randomAvailablePoint(0, 0, 0);	// get free point and check than [count] cell on the direction is free too
+    if (typeof(point) === "undefined" || point === null) {
+        console.log("Unable to find space for bonus");
+        return null;
+    }
+    var bonus = new Object();
+    var bonusIdx = Math.round(Math.random() * (bonusesTypes.length - 1));
+    bonusType = bonusesTypes[bonusIdx];
+    bonus.id = getNextObjId();
+    bonus.type = 'bonus';
+    bonus.body = [];
+    bonus.body.push(point);
+    bonus.score = bonusType.score;
+    bonus.grow = bonusType.grow;
+    bonus.color = bonusType.color;
+    bonusCount++;
+    gameobjs.push(bonus);
+    setFieldData(point, bonus.id, bonus.type, bonus.color);
+    return (bonus);
+}
+
+function deleteBonus(id) {
+    console.log("deleteBonus   id="+id);
+    bonusCount--;
+    var i = getObjIndexById(id);
+    delete gameobjs[i];
+}
+
+
+//-----------------------------------------------
+function newSnake(playerId, pColor) {
+    console.log("newSnake");
+    var snake = new Object();
+
+    snake.id = getNextObjId();
+    snake.player = playerId;
+    snake.type = 'snake';
+    snake.body = [];
+    snake.color = pColor;
+    snake.status = 'alive';
+    snake.direction = Math.floor(Math.random() * 3);
+    snake.update = snakeUpdate;
+    snake.grow = 0;
+
+    var point = randomAvailablePoint(snake.direction, 10, fieldWidth);	// get free point and check than [count] cell on the direction is free too
+    console.log("got random point: x=" + point.x + "  y=" + point.y);
+    if (typeof(point) === "undefined" || point === null) {
+        console.log("Unable to create snake for player");
+        throw "Unable to create snake for player";
+    }
+    var nextPoint = movePoint(point, snake.direction);
+    snake.body.push(nextPoint);
+    setFieldData(nextPoint, snake.id, snake.type, snake.color);
+    snake.body.push(point);
+    setFieldData(point, snake.id, snake.type, snake.color);
+    gameobjs.push(snake);
+
+    console.log("new snake created");
+    return (snake);
+}
+
+function deleteSnake(snake) {
+    console.log("deleteSnake   Removing snake's body cells: " + snake.body.length);
+    for (var point of snake.body) {
+        //console.log("clear   x="+point.x+"   y="+point.y)
+        setFieldData(point, 'empty', null, null);
+    }
+    for (var i = 0; i < gameobjs.length; i++) {
+        if (gameobjs[i] == snake) {
+            gameobjs.splice(i, 1);
+            break;
+        }
+    }
+}
+
+
+//-----------------------------------------------
+function newPlayer(userId, socketId) {
+    console.log("newPlayer");
+
+    var player = new Object();
+    try {
+        snake = newSnake(socketId, "red");
+        snake.player = player;
+        player.id = userId;
+        player.socket = socketId;
+        players.push(player);
+        player.snake = snake;
+        player.score = 0;
+        console.log("new player created");
+
+    }
+    catch (err) {
+        console.log("ERR: New player creation failed");
+        throw "New player creation failed <- " + err;
+    }
+    return (player);
+}
+
+function deletePlayer(player) {
+    console.log("deletePlayer");
+    console.log(player);
+    for (var p of players) {
+        if (p == player) {
+            console.log("player found");
+            try {
+                var msg = new Object();
+                msg.score = player.score;
+                io.sockets.connected[player.socket].emit("gameover", msg);
+            }
+            catch (err) {
+                console.log("WARN: user already disconnected");
+                console.log("WARN: " + err);
+            }
+            deleteSnake(p.snake);
+            players.splice(getPlayerId(player), 1);
+            //delete players[i];
+            updateGameStats();
+            break;
+        }
+    }
+}
+
+//-----------------------------------------------
+function getClearField() {
+    var clearField = new Array(fieldHeight * fieldWidth);
+    var emptyFieldData = new Object();
+    emptyFieldData.id = 'empty';
+    emptyFieldData.color = null;
+    clearField.fill(emptyFieldData);
+    console.log("field created");
+    return clearField;
+}
+
+
+function copySnakeToField(field, gameObjId) {
+    for (var i = 0; i < gameObjId[gameObjId].body.length; i++) {
+        field[gameObjId[gameObjId].body[i].x, gameObjId[gameObjId].body[i].y] = gameObjId;
+    }
+}
+
+
+function getRandomCellForSnake() {
+    point = new Point(Math.round(spawnMargin + (fieldWidth - spawnMargin * 2) * Math.random()),
+        Math.round(spawnMargin + (fieldHeight - spawnMargin * 2) * Math.random()));
+}
+
+
+function randomAvailablePoint(direction, count, margin) {
+    //console.log("randomAvailablePoint");
+    var point = new Object();
+    var deltas = getDeltas(direction)
+    point.x = Math.round(spawnMargin + (fieldWidth - spawnMargin * 2) * Math.random());
+    point.y = Math.round(spawnMargin + (fieldHeight - spawnMargin * 2) * Math.random());
+    var retryCount = 0;
+
+    while (!checkNextPoints(point, direction, count) && retryCount < 10) {
+        point.x = Math.round(spawnMargin + (fieldWidth - spawnMargin * 2) * Math.random());
+        point.y = Math.round(spawnMargin + (fieldHeight - spawnMargin * 2) * Math.random());
+        retryCount++;
+    }
+    if (retryCount === 10) {
+        console.log('unable to find free space');
+        return (null);
+    }
+    //console.log("randomAvailablePoint completed");
+    return (point);
+}
+
+
+function checkNextPoints(point, direction, count) {
+    var deltas = getDeltas(direction);
+    //var curPoint = point;
+    var curPoint=new Object();
+    curPoint.x=point.x;
+    curPoint.y=point.y;
+    for (var i = 0; i <= count; i++) {
+        if (fieldData(curPoint.x, curPoint.y) != 'empty') {
+            //console.log("free space check failed. Found: "+fieldData(curPoint.x,curPoint.y));
+            return (false);
+        }
+        else {
+            //console.log("free space check passed. Found: " + fieldData(curPoint.x, curPoint.y));
+        }
+        curPoint.x += deltas.deltaX;
+        curPoint.y += deltas.deltaY;
+    }
+    return (true);
+}
+
+
+function userId(userId, socketId) {
+    this.userId = userId;
+    this.socketId = socketId;
+    return this;
+}
+
+
+//-----------------------------------------------
+io.sockets.on('connection', function (socket) {
+    console.log('connection');
+
+    socket.on('client_init', function (data) {
+        console.log("client_init.  data: " + data);
+        console.log("playerid:" + data.id);
+        try {
+            var player = newPlayer(data.id, socket.id);
+            console.log('playerId: ' + data.id + '  socketId: ' + socket.id);
+            //servio.sockets.socket(id).emit('hello');
+            var msg = new Object();
+            msg.userId = data.id;
+            msg.socketId = socket.id;
+            msg.gameWidth = fieldWidth;
+            msg.gameHeight = fieldHeight;
+            msg.snakeId = player.snake.id;
+            socket.emit('playerid', msg);
+            updateGameStats();
+            sendField(socket);
+        }
+        catch (err) {
+            console.log("ERR: Unable to create player");
+            var msg = new Object();
+            msg.err = "Unable to create player <- " + err;
+            socket.emit('connecterr', msg);
+        }
+    });
+
+    socket.on('disconnect', function () {
+        console.log("disconnect");
+        var len = 0;
+        deletePlayer(getPlayerBySocket(socket));
+    });
+
+    socket.on('usrCtrl', function (data, id_player) {
+        console.log("usrCtrl");
+        p = getPlayerBySocket(socket);
+        if (typeof(p)!=='undefined' && p!==null) {
+            p.snake.direction = data;
+        }
+        else{
+            console.log("ERR: Player not found")
+        }
+    });
+
+    socket.on('pauseGame', function () {
+        console.log("pauseGame");
+        gamePaused = !gamePaused;
+    });
+
+    socket.on('freezeSnake', function () {
+        console.log("freezeSnake");
+        p = getPlayerBySocket(socket);
+        if (p.snake.status == 'frozen') {
+            p.snake.status = 'alive';
+        }
+        else {
+            p.snake.status = 'frozen';
+        }
+    });
+});
+
+
+//-----------------------------------------------
+function sendField(socket) {
+    socket.emit('gamefield', field);
+}
+
+
+function logUsersCount() {
+    console.log('usersCount: ' + players.length);
+}
+
+
+function getDeltas(direction) {
+    var deltas = new Object();
+    deltas.deltaX = 0;
+    deltas.deltaY = 0;
+    switch (direction) {
+        case 0:   // north
+            deltas.deltaY = -1;
+            break;
+        case 1:   // east
+            deltas.deltaX = 1;
+            break;
+        case 2:   // south
+            deltas.deltaY = 1;
+            break;
+        case 3:   // west
+            deltas.deltaX = -1;
+            break;
+    }
+    return (deltas);
+}
+
+
+function snakeUpdate(snake) {
+    //console.log("snakeUpdate #"+snake.id);
+    if (snake.status == 'frozen' || snake.status=='dead')
+        return;
+
+    var newHeadPoint = movePoint(snake.body[0],snake.direction);
+    //console.log("oldX="+snake.body[0].x+"  oldY="+snake.body[0].y+")   (newX="+newHeadPoint.x+"  newY="+newHeadPoint.y+")");
+
+    if (newHeadPoint.x >= fieldWidth || newHeadPoint.x < 0 || newHeadPoint.y >= fieldHeight || newHeadPoint.y < 0) {
+        // snake is dead
+        snake.status = 'dead';
+        console.log("Snake tried to escape and shot down");
+    }
+    else {
+        // check for barriers/objects and interact with them
+        //console.log(fieldData(newHeadPoint.x,newHeadPoint.y));
+        if (fieldData(newHeadPoint.x, newHeadPoint.y) != 'empty') {
+            //console.log(fieldData(newHeadPoint.x,newHeadPoint.y));
+            var gameObject = getGameObjectById(fieldData(newHeadPoint.x, newHeadPoint.y));
+            console.log('Getting object by id:' + fieldData(newHeadPoint.x, newHeadPoint.y));
+            switch (gameObject.type) {
+                case 'bonus':
+                    // snake should eat the bonus
+                    //var bonus = getGameObjectById(fieldData(newHeadPoint.x, newHeadPoint.y));
+                    console.log("snake #" + snake.id + " eats bonus  grow:" + gameObject.grow + "   score:" + gameObject.score);
+                    deleteBonus(gameObject.id);
+                    snake.player.score += gameObject.score;
+                    snake.grow = snake.grow + gameObject.grow;
+                    break;
+                case 'snake':
+                    if (gameObject.body[0].x==newHeadPoint.x && gameObject.body[0].y==newHeadPoint.y){
+                        gameObject.status=='dead';
+                        //deletePlayer(gameObject.snake.player);
+                    }
+                default:
+                    snake.status = 'dead';
+                    console.log("Snake found something and die. It found: " + fieldData(newHeadPoint.x, newHeadPoint.y));
+                    break;
+            }
+        }
+        if (snake.status !== 'dead') {
+            //if(fieldData(newHeadPoint.x,newHeadPoint.y)=='empty'){
+            // snake is alive and moving
+            //console.log("oldX="+snake.body[0].x+"  oldY="+snake.body[0].y+")   (newX="+newHeadPoint.x+"  newY="+newHeadPoint.y+")");
+            if (snake.grow == 0) {
+                var tailPoint = snake.body.pop();
+                setFieldData(tailPoint,'empty');
+            }
+            else {
+                //console.log("Snake is growing  grow="+snake.grow);
+                snake.grow--;
+            }
+            setFieldData(newHeadPoint, snake.id, snake.type, snake.color);
+            snake.body.unshift(newHeadPoint);
+        }
+    }
+
+/*
+    if (snake.status == 'dead') {
+        deletePlayer(snake.player)
+    }
+*/
+}
+
+
+function getGameObjectById(id) {
+    for (var i = 0; i < gameobjs.length; i++) {
+        if (gameobjs[i].id == id)
+            return gameobjs[i]
+    }
+}
+
+function setFieldData(point, id, type, color) {
+    var command = new Object();
+    command.point = point;
+    command.id = id;
+    command.type = type;
+    command.color = color;
+    commandsStack.push(command);
+}
+
+
+function processCommandStack() {
+    //console.log("processing commandsStack with "+commandsStack.length+" commands");
+    for (var i = 0; i < commandsStack.length; i++) {
+        //console.log(commandsStack[i]);
+        //var cellVal=new CellValue(commandsStack[i].id, commandsStack[i].color);
+        var cellVal = new Object();
+        cellVal.id = commandsStack[i].id;
+        cellVal.type = commandsStack[i].type;
+        cellVal.color = commandsStack[i].color;
+        //console.log(cellVal);
+        field[commandsStack[i].point.x + commandsStack[i].point.y * fieldWidth] = cellVal;
+    }
+}
+
+
+function getPlayerBySocket(socket) {
+    for (var p of players){
+    //for (var i = 0, len = players.length; i < len; ++i) {
+    //    var p = players[i];
+        if (p.socket == socket.id) {
+            return p;
+        }
+    }
+}
+
+
+function getPlayerId(player) {
+    for (var p of players) {
+        if (p === player)
+            return p;
+    }
+    return null;
+}
+
+
+function getObjIndexById(id) {
+    var i;
+    for (var gameobj of gameobjs) {
+        if (gameobj.id === id) {
+            return i;
+        }
+    }
+    console.log("ERR: getObjIndexById  id=" + id);
+    return null;
+}
+
+
+function fieldData(x, y) {
+    if (field[x + y * fieldWidth].id == 'empty') {
+        return 'empty';
+    }
+    else {
+        return (field[x + y * fieldWidth].id);
+    }
+}
+
+function updateGameStats() {
+    var stats = new Object();
+    stats.usersCount = players.length;
+    io.sockets.emit('gameStats', stats);
+
+}
+
+
+function updateGameData() {
+    //console.log("tick#"+tickNumber);
+    if (gamePaused)
+        return;
+
+    // update game data
+    for (var i = 0; i < gameobjs.length; i++) {
+        if (typeof(gameobjs[i].update) != "undefined") {
+            gameobjs[i].update(gameobjs[i]);
+        }
+    }
+
+    //generate new bonus
+    if (bonusCount <= bonusLimit && Math.random() * bonusDelay <= 1) {
+        newBonus();
+    }
+
+    // process commands in stack
+    processCommandStack();
+
+    for (var gameObject of gameobjs){
+        if (gameObject.type=='snake'){
+            if (gameObject.status=='dead')
+                deletePlayer(gameObject.player);
+        }
+    }
+
+    // process dead snake removal actions
+    processCommandStack();
+
+    // notify clients
+    var gameData = new Object();
+    gameData.commandsStack = commandsStack;
+    io.sockets.emit('gameData', gameData);
+    //console.log("Send commandsStack.length="+commandsStack.length);
+
+    // clear the stack
+    commandsStack = [];
+
+    tickNumber++;
+}
+
+setInterval(updateGameData, gameUpdateDelay);
---
+++ public/client.js
@@ -0,0 +1,204 @@
+var socket;
+var userName = "";
+var gameWidth = 0;
+var gameHeight = 0;
+var canvas;
+var cellSize = 10;
+var gameFieldColor = "#00005f";
+var gridColor="rgba(100,100,128,1)";
+var snakeId;
+var field;
+
+function sendUserControl(msg) {
+    socket.emit('usrCtrl', msg);
+}
+
+function sendFreezeSnake() {
+    socket.emit('freezeSnake');
+}
+
+function sendPauseGame() {
+    socket.emit('pauseGame');
+}
+
+$(document).keydown(function (e) {
+    switch (e.keyCode) {
+        case 40:
+            sendUserControl(2);
+            break;
+        case 39:
+            sendUserControl(1);
+            break;
+        case 38:
+            sendUserControl(0);
+            break;
+        case 37:
+            sendUserControl(3);
+            break;
+        case 80 : //p
+            sendFreezeSnake();
+            break;
+        case 79 : //o
+            sendPauseGame();
+            break;
+    }
+});
+
+
+$('form').submit(function () {
+    socket = io();
+    setupSocket(socket);
+
+    console.log("form submit");
+    var msg = new Object();
+    msg.id = $('#username').val();
+    socket.emit('client_init', msg);
+    return false;
+});
+
+
+function drawEmptyGameField() {
+    console.log("drawEmptyGameField");
+    canvas = document.getElementById("gameCanvas");
+    var cellH=Math.floor((window.innerHeight-2)/(gameHeight));
+    var cellW=Math.floor((window.innerWidth-2)/(gameWidth));
+    cellSize= cellH<cellW ? cellH : cellW;
+    canvas.width = gameWidth*cellSize;
+    canvas.height = gameHeight*cellSize;
+    canvas.style.margin="auto";
+    //document.getElementById("game").style.display = "block";
+    //document.getElementById("game").style.verticalAlign = "middle";
+
+    var ctx = canvas.getContext('2d');
+    ctx.beginPath();
+    ctx.strokeStyle = gridColor;
+    ctx.lineWidth = "1";
+    for (var i = 1; i < gameHeight; i++) {
+        ctx.moveTo(0, i * cellSize);
+        ctx.lineTo(gameWidth * cellSize, i * cellSize);
+    }
+    for (var i = 1; i < gameWidth; i++) {
+        ctx.moveTo(i * cellSize, 0);
+        ctx.lineTo(i * cellSize, gameHeight * cellSize);
+    }
+    ctx.stroke();
+}
+
+function drawData(x, y, id, type, color) {
+    canvas = document.getElementById("gameCanvas");
+    var ctx = canvas.getContext('2d');
+    if (id == 'empty') {
+        //console.log("clearing x="+x+" y="+y);
+        ctx.beginPath();
+        ctx.fillStyle = gameFieldColor;
+        ctx.strokeStyle = gridColor;
+        ctx.lineWidth = "1";
+        ctx.rect(cellSize * x, cellSize * y, cellSize, cellSize);
+        ctx.fill();
+        ctx.stroke();
+    }
+    else {
+        if (id == snakeId) {
+            ctx.beginPath();
+            //console.log("drawing x="+commandsStack[i].point.x+" y="+commandsStack[i].point.y);
+            ctx.fillStyle = "red";
+            ctx.strokeStyle = gridColor;
+            ctx.lineWidth = "1";
+            ctx.rect(cellSize * x, cellSize * y, cellSize, cellSize);
+            ctx.fill();
+            ctx.stroke();
+        }
+        else {
+            ctx.beginPath();
+            //console.log("drawing x="+commandsStack[i].point.x+" y="+commandsStack[i].point.y+"  color:"+commandsStack[i].color);
+            if (type=='snake') {
+                ctx.fillStyle = "lightgrey";
+            }
+            else {
+                ctx.fillStyle = color;
+            }
+
+            ctx.strokeStyle = gridColor;
+            ctx.lineWidth = "1";
+
+            if (type=='bonus'){
+                ctx.arc(cellSize*x+cellSize/2, cellSize * y+cellSize/2, cellSize/2-2, 0, 2*Math.PI);
+            }
+            else {
+                ctx.rect(cellSize*x, cellSize * y, cellSize, cellSize);
+            }
+            ctx.fill();
+            ctx.stroke();
+        }
+
+    }
+}
+
+function drawGameField() {
+    for (var i = 0; i < gameHeight; i++) {
+        for (var j = 0; j < gameWidth; j++) {
+            drawData(j, i, field[j + i * gameWidth].id, field[j + i * gameWidth].type, field[j + i * gameWidth].color);
+        }
+    }
+}
+
+
+function setupSocket(socket) {
+    socket.on('correct', function (msg) {
+        console.log('correct: ' + msg);
+    });
+    socket.on('playerid', function (msg) {
+        console.log('playerid:' + msg.userId + "  gameWidth=" + msg.gameWidth + "  gameHeight=" + msg.gameHeight);
+        userName = msg.userId;
+        gameWidth = msg.gameWidth;
+        gameHeight = msg.gameHeight;
+        snakeId = msg.snakeId;
+        document.getElementById("game").style.display = "block";
+        document.getElementById("login").style.display = "none";
+        //document.getElementById("playerIdContainer").innerHTML = '<p>UserId: ' + msg.userId + '<br>SocketId: ' + msg.socketId + '</p>';
+        document.getElementById("playerIdContainer").innerHTML = '<p>UserId: ' + msg.userId +'</p>';
+        drawEmptyGameField();
+    });
+
+    socket.on('gameover', function (msg) {
+        console.log("gameover:  msg.score=" + msg.score);
+        socket.disconnect();
+        alert("\nGame over.\n\nYour score: " + msg.score + "\n\n");
+        document.getElementById("game").style.display = "none";
+        document.getElementById("login").style.display = "flex";
+        document.getElementById("username").focus();
+    });
+
+    socket.on('connecterr', function (msg) {
+        console.log('connecterr:' + msg.err);
+        alert("Connection error: " + msg.err);
+        document.getElementById("game").style.display = "none";
+        document.getElementById("login").style.display = "flex";
+    });
+
+    socket.on('gameStats', function (stats) {
+        console.log('usersCount:' + stats.usersCount);
+        document.getElementById("usersCount").innerText = stats.usersCount;
+    });
+
+    socket.on('gamefield', function (gamefield) {
+        console.log('gamefield received');
+        field = gamefield;
+        drawGameField();
+    });
+
+    socket.on('gameData', function (gameData) {
+        //console.log("Received data:");
+        //console.log(gameData);
+        canvas = document.getElementById("gameCanvas");
+        var ctx = canvas.getContext('2d');
+        var commandsStack = gameData.commandsStack;
+        //console.log("Commands stack length:"+commandsStack.length);
+        for (var i = 0; i < commandsStack.length; i++) {
+            //console.log(commandsStack[i]);
+            drawData(commandsStack[i].point.x, commandsStack[i].point.y, commandsStack[i].id, commandsStack[i].type , commandsStack[i].color);
+        }
+        //console.log("gameData done");
+    });
+}
+
